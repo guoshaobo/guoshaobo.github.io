27017aff5ab19330a9cb39e8e845329e04653791
guoshaobo <827571603@qq.com>
2018-01-06 00:41:11 +0800

    11

--- zfb.php
+++ zfb.php
@@ -14,24 +14,3 @@
 
 </html>
 
-<!-- <script>
-function isMobile(){
-	var sUserAgent=navigator.userAgent.toLowerCase();
-	var bIsIpad=sUserAgent.match(/ipad/i)=="ipad";
-	var bIsIphoneOs=sUserAgent.match(/iphone os/i)=="iphone os";
-	var bIsMidp=sUserAgent.match(/midp/i)=="midp";
-	var bIsUc7=sUserAgent.match(/rv:1.2.3.4/i)=="rv:1.2.3.4";
-	var bIsUc=sUserAgent.match(/ucweb/i)=="ucweb";
-	var bIsAndroid=sUserAgent.match(/android/i)=="android";
-	var bIsCE=sUserAgent.match(/windows ce/i)=="windows ce";
-	var bIsWM=sUserAgent.match(/windows mobile/i)=="windows mobile";
-	return bIsIpad||bIsIphoneOs||bIsMidp||bIsUc7||bIsUc||bIsAndroid||bIsCE||bIsWM}
-	</script>
-	<script src="https://open.mobile.qq.com/sdk/qqapi.js?_bid=152"></script>
-	<script type="text/javascript">
-	if(isMobile()){
-		mqq.ui.openUrl({
-			target:2,url:"alipays://platformapi/startapp?saId=10000007&clientVersion=3.7.0.0718&qrcode=' . urlencode($url) . '"})
-	};
-	mqq.ui.addShortcut({action:"web",title:"",});
-	</script> -->
